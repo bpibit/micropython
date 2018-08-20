c57cab807efc2f1432da34086f12b29619dfc522
junhuanchen <junhuanchen@qq.com>
2018-08-20 15:19:47 +0800

    add more output info about webdav

--- ports/esp32/modules/inisetup.py
+++ ports/esp32/modules/inisetup.py
@@ -43,10 +43,10 @@ import network
 print('network smartconfig')
 network.smartconfig()
 import time
-time.sleep(4)
+time.sleep(3)
 import webdav
 print('webdav start')
-webdav.start()
+print(webdav.start())
 import os
 print('check index.py')
 os.stat('index.py')
