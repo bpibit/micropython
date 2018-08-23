484fe9d29c8b6b2a1950aa2a7562b2e1e8a036f7
bpibit <bpibit@outlook.com>
2018-08-24 02:49:53 +0800

    fix webdav for CTRL-C

--- ports/esp32/modules/webdav.py
+++ ports/esp32/modules/webdav.py
@@ -1,31 +1,39 @@
 import mongoose
+import _thread
+import time
+import network
 
 WebDavState = False
+lock=_thread.allocate_lock()
 
-def __poll__():
-    import time
+def __poll__(delay):
     global WebDavState
     while WebDavState:
-        try:
-            time.sleep(0.5)
+        #time.sleep(delay)
+        if lock.acquire():
             mongoose.poll()
-        except KeyboardInterrupt:
-            pass
+            lock.release()
+
+ #       try:
+ #           time.sleep(delay)
+ #           mongoose.poll()
+ #       except KeyboardInterrupt:
+ #           pass
             
 def start():
-    import _thread
-    import network
     if(network.WLAN(network.STA_IF).isconnected()):
         global WebDavState
         if(False == WebDavState):
             mongoose.init()
             WebDavState = True
             _thread.stack_size(8 * 1024)
-            _thread.start_new_thread(__poll__, ())
+            _thread.start_new_thread(__poll__, (0.5,))
             _thread.stack_size()
             return True
     return False
 
 def close():
     global WebDavState
-    WebDavState = False
+    if (WebDavState == True):
+        _thread.exit()
+        WebDavState = False
