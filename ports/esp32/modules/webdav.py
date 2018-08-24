7c3ee0bfaa6d4ebe14c5d219e3c1e45e7354480c
bpibit <bpibit@outlook.com>
2018-08-24 16:07:51 +0800

    webdav.close()

--- ports/esp32/modules/webdav.py
+++ ports/esp32/modules/webdav.py
@@ -4,13 +4,17 @@ import time
 import network
 
 WebDavState = False
+RequestExit = False
 lock=_thread.allocate_lock()
 
 def __poll__(delay):
-    global WebDavState
+    global WebDavState, RequestExit
     while WebDavState:
         #time.sleep(delay)
         if lock.acquire():
+            if(RequestExit == True):
+                RequestExit == False
+                _thread.exit()
             mongoose.poll()
             lock.release()
 
@@ -21,19 +25,27 @@ def __poll__(delay):
  #           pass
             
 def start():
-    if(network.WLAN(network.STA_IF).isconnected()):
-        global WebDavState
-        if(False == WebDavState):
-            mongoose.init()
-            WebDavState = True
-            _thread.stack_size(8 * 1024)
-            _thread.start_new_thread(__poll__, (0.5,))
-            _thread.stack_size()
-            return True
-    return False
+    global WebDavState, RequestExit
+    if(WebDavState == False):
+        #if(network.WLAN(network.STA_IF).isconnected()):
+        #if(False == WebDavState):
+        mongoose.init()
+        WebDavState = True
+        _thread.stack_size(8 * 1024)
+        _thread.start_new_thread(__poll__, (0.5,))
+        _thread.stack_size()
+    return WebDavState
 
 def close():
-    global WebDavState
+    global WebDavState, RequestExit
     if (WebDavState == True):
-        _thread.exit()
         WebDavState = False
+        RequestExit = True        
+        #while (True):
+        #    if(RequestExit == False):
+        #        break
+        #    if(not lock.locked()):
+        #        break
+        #_thread.exit()
+
+
